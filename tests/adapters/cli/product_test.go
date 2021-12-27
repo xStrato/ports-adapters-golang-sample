c81b3a349fa1c26d60af3509884625c41d4a34bc
xStrato <metalofgear@hotmail.com>
2021-12-27 19:15:00 -0300

    update

--- tests/adapters/cli/product_test.go
+++ tests/adapters/cli/product_test.go
@@ -11,29 +11,57 @@ import (
 	"github.com/xStrato/ports-adapters-go-sample/src/application/entities"
 )
 
-func TestProduct(t *testing.T) {
+func TestProductRun(t *testing.T) {
+	ctrl := gomock.NewController(t)
+	defer ctrl.Finish()
 
-	t.Run("Run_CreateValidProduct_ShouldReturnNil", func(t *testing.T) {
+	id := "cba"
+	name := "PS6"
+	status := "Disabled"
+	price := 59.99
+	product := entities.NewProductFrom(id, name, status, float32(price))
+
+	service := mocks.NewMockIProductService(ctrl)
+	service.EXPECT().Create(name, float32(price)).Return(product, nil).AnyTimes()
+	service.EXPECT().Get(id).Return(product, nil).AnyTimes()
+	service.EXPECT().Enable(gomock.Any()).Return(product, nil).AnyTimes()
+	service.EXPECT().Disable(gomock.Any()).Return(product, nil).AnyTimes()
+
+	t.Run("Create_ValidProduct_ShouldReturnNil", func(t *testing.T) {
 		//Arrange
-		ctrl := gomock.NewController(t)
-		defer ctrl.Finish()
-
-		id := "cba"
-		name := "PS6"
-		status := "Disabled"
-		price := 59.99
-
-		product := entities.NewProductFrom(id, name, status, float32(price))
-
-		service := mocks.NewMockIProductService(ctrl)
-		service.EXPECT().Create(name, float32(price)).Return(product, nil).AnyTimes()
-		service.EXPECT().Get(id).Return(product, nil).AnyTimes()
-		service.EXPECT().Enable(gomock.Any()).Return(product, nil).AnyTimes()
-		service.EXPECT().Disable(gomock.Any()).Return(product, nil).AnyTimes()
-
 		expedtedResult := fmt.Sprintf("Product created!! Id: %s, Name: %s, Price: %f, Status: %s", id, name, float32(price), status)
 		//Act
 		result, err := cli.Run(service, "create", "", name, float32(price))
+		//Assert
+		require.Nil(t, err)
+		require.Equal(t, expedtedResult, result)
+	})
+
+	t.Run("Enable_ValidProduct_ShouldReturnNil", func(t *testing.T) {
+		//Arrange
+		expedtedResult := fmt.Sprintf("Product %s has been enabled", name)
+		//Act
+		result, err := cli.Run(service, "enable", id, name, float32(price))
+		//Assert
+		require.Nil(t, err)
+		require.Equal(t, expedtedResult, result)
+	})
+
+	t.Run("Disable_ValidProduct_ShouldReturnNil", func(t *testing.T) {
+		//Arrange
+		expedtedResult := fmt.Sprintf("Product %s has been disabled", name)
+		//Act
+		result, err := cli.Run(service, "disable", id, name, float32(price))
+		//Assert
+		require.Nil(t, err)
+		require.Equal(t, expedtedResult, result)
+	})
+
+	t.Run("Default_ValidProduct_ShouldReturnNil", func(t *testing.T) {
+		//Arrange
+		expedtedResult := fmt.Sprintf("Product Id: %s\n Name: %s\n Price: %f\n Status: %s", id, name, float32(price), status)
+		//Act
+		result, err := cli.Run(service, "", id, name, float32(price))
 		//Assert
 		require.Nil(t, err)
 		require.Equal(t, expedtedResult, result)
