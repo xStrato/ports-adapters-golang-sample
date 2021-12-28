7dd84476ccca8f6085d132cca4a37ddb9ea05f5b
xStrato <metalofgear@hotmail.com>
2021-12-28 11:53:33 -0300

    update

--- src/adapters/web/handlers/product_handler.go
+++ src/adapters/web/handlers/product_handler.go
@@ -8,11 +8,11 @@ import (
 	"github.com/urfave/negroni"
 	"github.com/xStrato/ports-adapters-go-sample/src/adapters/dtos"
 	"github.com/xStrato/ports-adapters-go-sample/src/application/interfaces"
-	"golang.org/x/tools/go/analysis/passes/nilfunc"
 )
 
 func MakeProductHandler(r *mux.Router, n *negroni.Negroni, s interfaces.IProductService) {
 	r.Handle("/product/{id}", n.With(negroni.Wrap(getProduct(s)))).Methods("GET", "OPTIONS")
+	r.Handle("/product", n.With(negroni.Wrap(createProduct(s)))).Methods("POST", "OPTIONS")
 }
 
 func getProduct(s interfaces.IProductService) http.Handler {
@@ -33,7 +33,7 @@ func getProduct(s interfaces.IProductService) http.Handler {
 	})
 }
 
-func createProduct(s interfaces.IProductService)  http.Handler {
+func createProduct(s interfaces.IProductService) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		w.Header().Set("Content-Type", "application/json")
 		var pDto dtos.Product
@@ -51,11 +51,11 @@ func createProduct(s interfaces.IProductService)  http.Handler {
 			return
 		}
 
-		err := json.NewEncoder(w).Encode(p)
+		err = json.NewEncoder(w).Encode(p)
 		if err != nil {
 			w.WriteHeader(http.StatusInternalServerError)
 			w.Write(jsonError(err.Error()))
 			return
 		}
-	}
+	})
 }
